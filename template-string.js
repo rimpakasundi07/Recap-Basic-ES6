38f3734717b27e399474ac4e7887d82e3e55d9da
Rimpa Kasundi <rimpakasundi@gmail.com>
2025-09-04 20:57:38 +0530

    Add template literal  showing multi-line strings

---
+++ template-string.js
@@ -0,0 +1,18 @@
+const poem =
+  "amm pata jora jora \n" +
+  "marbo chabuk chorbo ghora\n" +
+  "ore biwi sore dara\n" +
+  "asche amar pagla ghora\n";
+console.log(poem);
+
+// --------------------------->>>>>>>>>>>>>>
+const kobita = `amm pata jora jora 
+  marbo chabuk chorbo ghora
+  ore biwi sore dara
+  asche amar pagla ghora`;
+console.log(kobita);
+
+// ------------------>>>>>>>>>>>>>>>>>>>>>>
+const price = 100;
+const discountedPrice = `Discounted Price is after 20% discount ${price * 0.8}`;
+console.log(discountedPrice);
