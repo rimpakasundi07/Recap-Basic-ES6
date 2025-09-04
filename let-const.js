ce64a96c075dc18e727a48294e40a3a602eea05c
Rimpa Kasundi <rimpakasundi@gmail.com>
2025-09-04 19:55:25 +0530

    Update student marks  in JavaScript object

---
+++ let-const.js
@@ -0,0 +1,8 @@
+const dress = ["Shirt", "pant", "jacket"];
+dress.push("maflar");
+console.log(dress);
+
+// -------------------------->>>>>>>>>>>
+const student = { name: "jaga", marks: 60 };
+student.marks = 70;
+console.log(student);
